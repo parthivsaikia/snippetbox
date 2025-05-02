2b79b1de7c51191658877085dece0dd2ede08882
parthivsaikia <parthivsaikia985@gmail.com>
2025-05-02 07:22:48 +0530

    backup before arch install

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -44,7 +44,7 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", "POST")
-		http.Error(w, "Method Not Allowed", 405)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
 	w.Write([]byte("Create a new snippet."))
